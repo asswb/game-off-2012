93a38a979dd1af5858aee2347851ca9ee2b7ee44
Seppi <josefnpat@hotmail.com>
2012-11-01 23:08:25 -0400

    Bootstraping the login and regiser forms some more.

--- www/inc/forms.php
+++ www/inc/forms.php
@@ -2,20 +2,29 @@
 
 function login_form(){
   ?>
-  <div>
-    <form action="" method="POST">
-      <input type="hidden" id="exe" name="exe" value="login" />
-      <div>Username:</div>
-      <div><input type="text" class="" id="username" name="username" value="" /></div>
-      <div>Password</div>
-      <div><input type="password" class="" id="password" name="password" value="" /></div>
-      <div><input type="submit" class="" id="login" name="login" value="Login" /></div>
-    </form>
-    <form action="" method="POST">
-     <input type="hidden" id="page" name="page" value="register" />
-     <div><input type="submit" id="register" name="register" value="Register Here" /></div>
-    </form>
+<form class="form-horizontal" method="post" action="">
+  <input type="hidden" name="exe" value="login" />
+  <div class="control-group">
+    <label class="control-label">Username</label>
+    <div class="controls">
+      <input type="text" name="username" placeholder="Username" />
+    </div>
   </div>
+  <div class="control-group">
+    <label class="control-label">Password</label>
+    <div class="controls">
+      <input type="password" name="password" placeholder="Password" />
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">
+      <button type="submit" class="btn btn-primary">Login</button>
+    </div>
+  </div>
+</form>
+
+<a class="btn" href="?page=register">Register Here</a>
+
   <?php
 }
 
@@ -26,25 +35,25 @@ function register_form(){
   <div class="control-group">
     <label class="control-label">Username</label>
     <div class="controls">
-      <input type="text" name="username" placeholder="Username"<?php if(isset($_POST['username'])){echo " value=\"".$_POST['username']."\"";} ?>>
+      <input type="text" name="username" placeholder="Username"<?php if(isset($_POST['username'])){echo " value=\"".$_POST['username']."\"";} ?> />
     </div>
   </div>
   <div class="control-group">
     <label class="control-label">Password</label>
     <div class="controls">
-      <input type="password" name="password1" placeholder="Password">
+      <input type="password" name="password1" placeholder="Password" />
     </div>
   </div>
   <div class="control-group">
     <label class="control-label">Confirm Password</label>
     <div class="controls">
-      <input type="password" name="password2" placeholder="Password">
+      <input type="password" name="password2" placeholder="Password" />
     </div>
   </div>
   <div class="control-group">
     <label class="control-label">Repository Name</label>
     <div class="controls">
-      <input type="text" name="repo_name" value="<?php echo (isset($_POST['repo_name'])?$_POST['repo_name']:"GitHub Tycoon Game"); ?>">
+      <input type="text" name="repo_name" value="<?php echo (isset($_POST['repo_name'])?$_POST['repo_name']:"GitHub Tycoon Game"); ?>" />
     </div>
   </div>
   <div class="control-group">
