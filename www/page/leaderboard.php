9ce86f36e946af346068b2eadd68b78f51066d90
Seppi <josefnpat@hotmail.com>
2012-11-01 22:26:42 -0400

    closes #8

--- www/page/leaderboard.php
+++ www/page/leaderboard.php
@@ -1,6 +1,37 @@
-<pre><?php
-
-foreach($db->query('SELECT * FROM users') as $row){
-  print_r($row);
+<h2>Leaderboard</h2>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Rank</th>
+      <th>User Name</th>
+      <th>Repo Name</th>
+      <th>Score</th>
+    </tr>
+  </thead>
+<?php
+$q = $db->query('select `username`,`repo_name`,(com*cbq) as score from users order by score DESC');
+$rank = 0;
+while($row = $q->fetch(PDO::FETCH_ASSOC)){
+  $rank++;
+?>
+  <tbody>
+    <tr>
+      <td>#<?php echo $rank; ?></td>
+      <td><?php echo $row['username']; ?></td>
+      <td><?php echo $row['repo_name']; ?></td>
+      <td><?php echo $row['score']; ?></td>
+    </tr>
+  </tbody>
+<?php
 }
-?></pre>
+if($rank == 0){
+?>
+  <tbody>
+    <tr>
+      <td>No players yet.</td>
+    </tr>
+  </tbody>
+<?php
+}
+?>
+</table>
