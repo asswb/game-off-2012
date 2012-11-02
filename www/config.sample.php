ee3d3cec8f36a4cd93ab762a002e88b943a0b2b5
Seppi <seppi@josefnpat.com>
2012-11-01 23:15:43 -0300

    whitespace

--- www/config.sample.php
+++ www/config.sample.php
@@ -1,7 +1,7 @@
 <?php
 define("DEBUG",FALSE);
 
-define("DB_HOST","localhost");// Database host
-define("DB_NAME,"");// Database name
-define("DB_USER","");// Database username
-define("DB_PASS","");// Database password
+define("DB_HOST","localhost"); // Database host
+define("DB_NAME,""); // Database name
+define("DB_USER",""); // Database username
+define("DB_PASS",""); // Database password
