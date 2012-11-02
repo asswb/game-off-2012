600b7d2e3f42a021bcd94cc557fe1efefbb022ea
pstanley <pstanley@pstanley-desktop.(none)>
2012-11-01 21:31:11 -0400

    * Corrected the page call on the login form. * Added the "Register" button to the page.

--- www/page/login.php
+++ www/page/login.php
@@ -4,14 +4,16 @@
   </head>
   <body>
     <div>
-      <form action='<?=$_SERVER["PHP_SELF"]?>' method='POST'>
-        <input type='hidden' id='page' name='page' value='main' />
-        <input type='hidden' id='exe' name='exe' value='login' />
+      <form action='<?=$_SERVER["PHP_SELF"]?>?page=main' method='POST'>
         <div>Username:</div>
         <div><input type='text' class='' id='username' name='username' value='' /></div>
         <div>Password</div>
         <div><input type='password' class='' id='password' name='password' value='' /></div>
-        <div><input type='submit' class='' id='login' name='login' value='Login' /></div>
+        <div><input type='submit' class='' id='exe' name='exe' value='Login' /></div>
+      </form>
+      <form action='<?=$_SERVER["PHP_SELF"]?>?page=register' method='POST'>
+       <div><input type='submit' id='register' name='register' value='Register Here' disabled/></div>
+      </form>
    </div>
   </body>
 </html> 
