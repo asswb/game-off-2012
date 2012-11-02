0bd4ea94f054e81d9fe2272f47b6c633e41bf1af
peterrstanley <peterrstanley@gmail.com>
2012-11-01 23:11:26 -0400

    *Added Salt constant in to config template.

--- www/config.sample.php
+++ www/config.sample.php
@@ -5,3 +5,5 @@ define("DB_HOST","localhost"); // Database host
 define("DB_NAME",""); // Database name
 define("DB_USER",""); // Database username
 define("DB_PASS",""); // Database password
+
+define("SYS_SALT",""); // Password Salt
