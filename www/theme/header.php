370a2856ea857452f99cbada4f05f03cac1edf14
Seppi <josefnpat@hotmail.com>
2012-11-04 02:16:51 -0500

    Adding style link

--- www/theme/header.php
+++ www/theme/header.php
@@ -7,8 +7,8 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <!-- Le styles -->
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <style>
       body {
         padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
