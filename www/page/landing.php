c96a3de3d2b0da1c1d69cb15df253d469d4d272b
Seppi <josefnpat@hotmail.com>
2012-11-04 00:31:10 -0400

    Add more info for a new user on landing page.

--- www/page/landing.php
+++ www/page/landing.php
@@ -1,10 +1,20 @@
 <?php
 if(isset($user->session)){
 ?>
-  <h4>I hope you have a nice day. -Seppi</h4>
-  <h4>Eat Dirt. -Amanda</h4>
+<h2>Welcome to GitHub Tycoon!</h2>
+<p>Welcome to the Beta version!</p>
+<p>This game is brought to you by <a href="https://github.com/josefnpat">josefnpat</a> and <a href="https://github.com/peterrstanley">peterrstanley</a>.</p>
+<p>Technologies used;</p>
+<ul>
+<li>PHP</li>
+<li>MySQL</li>
+<li>Twitter Bootstrap</li>
+</ul>
 <?php
 } else {
+?>
+<h4>Welcome to GitHub Tycoon! Please login or register to play!</h4>
+<?php
   login_form();
 }
 ?>
