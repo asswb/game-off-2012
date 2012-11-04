e61c25df6bacc988bba762a3ed1f2e947d6f70ba
Seppi <josefnpat@hotmail.com>
2012-11-04 03:38:35 -0500

    Adding escape db quotes

--- www/inc/issue.php
+++ www/inc/issue.php
@@ -13,9 +13,9 @@ class issues
 
   public function get_current_issues(){
     $return_array = array();
-    $q = $this->db->query('SELECT issue,issue_eta FROM users WHERE userid="'.$this->uid.'"');
+    $q = $this->db->query('SELECT issue,issue_eta FROM users WHERE userid='.$this->db->quote($this->uid));
     while($row = $q->fetch(PDO::FETCH_ASSOC)){
-      $q2 = $this->db->query('SELECT name,description,type FROM issue_table WHERE iid="'.$row["issue"].'"');
+      $q2 = $this->db->query('SELECT name,description,type FROM issue_table WHERE iid='.$this->db->quote($row["issue"]));
       $row2 = $q2->fetch(PDO::FETCH_ASSOC);
       $return_array['iid'] = $row["issue"];
       $return_array['name'] = $row2["name"];
@@ -28,7 +28,7 @@ class issues
 
   public function get_issue_detail($iid){
     $return_array = array();
-    $q2 = $this->db->query('SELECT name,description,type FROM issue_table WHERE iid="'.$iid.'"');
+    $q2 = $this->db->query('SELECT name,description,type FROM issue_table WHERE iid='.$this->db->quote($iid));
     $row2 = $q2->fetch(PDO::FETCH_ASSOC);
     $return_array['iid'] = $row["issue"];
     $return_array['name'] = $row2["name"];
@@ -41,15 +41,15 @@ class issues
 
   public function set_current_issue($iid){
     $eta = calculate_eta($iid);
-    $this->db->query('UPDATE users SET issue="'.$iid.'",issue_eta="'.$eta.'" WHERE uid="'.$this->uid.'"');
+    $this->db->query('UPDATE users SET issue='.$this->db->quote($iid).',issue_eta='.$this->db->quote($eta).' WHERE uid='.$this->db->quote($this->uid));
   }
 
   private function calculate_eta($iid){
 
-    $q = $this->db->query('SELECT cbq FROM users WHERE userid="'.$this->uid.'"');
+    $q = $this->db->query('SELECT cbq FROM users WHERE userid='.$this->db->quote($this->uid));
     $row = $q->fetch(PDO::FETCH_ASSOC);
 
-    $q2 = $this->db->query('SELECT time FROM issue_table WHERE iid="'.$iid.'"');
+    $q2 = $this->db->query('SELECT time FROM issue_table WHERE iid='.$this->db->quote($iid));
     $row2 = $q2->fetch(PDO::FETCH_ASSOC);
 
     return ($row['cbq'] * $row2['time']);
