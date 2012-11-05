a164e3b5c80736d0eadd9b31968e5eb046887b01
Seppi <josefnpat@hotmail.com>
2012-11-05 06:08:50 -0500

    Adding start of issue commands

---
+++ www/exe/issue-start.php
@@ -0,0 +1,4 @@
+<?php
+if(isset($user->session)){
+  $alert->add("Issue Start","The issue has been started.","success");
+}
---
+++ www/exe/issue-wontfix.php
@@ -0,0 +1,4 @@
+<?php
+if(isset($user->session)){
+  $alert->add("Issue Wontfix","The issue has been marked as wontfix.","success");
+}
