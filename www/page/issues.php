08d1591d2148083a6313aa46f240945a2ee53a5a
Seppi <josefnpat@hotmail.com>
2012-11-05 12:49:32 -0500

    closes #26

--- www/page/issues.php
+++ www/page/issues.php
@@ -7,12 +7,33 @@ if(isset($user->session)){
     $max = $issues->calculate_eta($current_issue);
     $current = $user->data['issue_eta']-time();
     $percent = floor((1-$current/$max)*100);
+    $remain = $user->data['issue_eta'] - time();
 ?>
 <h4>Working on issue `<?php echo $current_issue['name']; ?>`.</h4>
-<p><?php echo $issues->stostr($user->data['issue_eta'] - time()); ?> remain.</p>
-<div class="progress progress-striped">
-  <div class="bar" style="width: <?php echo $percent; ?>%;"><?php echo $percent; ?>%</div>
+<p id="issprg_text"><span id="issprg_counter"><?php echo $remain; ?></span> seconds remain.</p>
+<div id="issprg_bar_style" class="progress progress-striped">
+  <div id="issprg_bar" class="bar" style="width: <?php echo $percent; ?>%;"><?php echo $percent; ?>%</div>
 </div>
+
+<script>
+  var issprg_seconds = <?php echo $current; ?>;
+
+  function issprg_display(){
+    issprg_seconds--;
+    if(issprg_seconds>=0){
+      document.getElementById("issprg_counter").innerHTML=issprg_seconds;
+      document.getElementById("issprg_bar").style.width=(100-Math.floor(issprg_seconds/<?php echo $max; ?>*100))+"%";
+      document.getElementById("issprg_bar").innerHTML=document.getElementById("issprg_bar").style.width;
+      setTimeout("issprg_display()",1000);
+    } else {
+      document.getElementById("issprg_text").innerHTML='Issue Complete';
+      document.getElementById("issprg_bar_style").className='progress progress-success progress-striped';
+      document.getElementById("issprg_bar").style.width='100%';
+    }
+  }
+  issprg_display();
+</script>
+
 <?php
   } else {
 ?>
