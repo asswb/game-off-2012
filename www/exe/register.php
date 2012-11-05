24cf706730529639dd1e99c2cbaa54ebe423048e
Seppi <josefnpat@hotmail.com>
2012-11-05 05:46:28 -0500

    Don't let sessioned users register

--- www/exe/register.php
+++ www/exe/register.php
@@ -1,6 +1,10 @@
 <?php
-
-if($user->register($_POST['username'],$_POST['password1'],$_POST['password2'],$_POST['repo_name'])){
-  $user->login($_POST['username'],$_POST['password1']);
+if(isset($user->session)){
+  $alert->add("Cannot Register With User Session","You are currently logged in, and need to be logged out to register.","info");
   $page="landing";
+} else {
+  if($user->register($_POST['username'],$_POST['password1'],$_POST['password2'],$_POST['repo_name'])){
+    $user->login($_POST['username'],$_POST['password1']);
+    $page="landing";
+  }
 }
