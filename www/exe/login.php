8ad630a034e449103bb60fa537d00ef9059ce871
Seppi <josefnpat@hotmail.com>
2012-11-05 05:50:50 -0500

    Do not log user in if they have already logged in.

--- www/exe/login.php
+++ www/exe/login.php
@@ -1,3 +1,7 @@
 <?php
-$user->login($_POST['username'],$_POST['password']);
+if(isset($user->session)){
+  $alert->add("User Session Exists","You are currently logged in! Please log out first to change accounts.");
+} else {
+  $user->login($_POST['username'],$_POST['password']);
+}
 $page = "landing";
