865b8dbf4d709ade3546be5dfe67db1de0e872e1
Seppi <josefnpat@hotmail.com>
2012-11-05 09:15:56 -0500

    Closes #23

--- www/exe/issue-wontfix.php
+++ www/exe/issue-wontfix.php
@@ -1,4 +1,14 @@
 <?php
 if(isset($user->session)){
-  $alert->add("Wontfix Issue","The issue has been marked as wontfix.","success");
+  if($user->data['issue']){
+    $alert->add("Cannot Wontfix Issue","There is already an active issue.","info");
+  } else {
+    $issue = $issues->get_user_issue($_POST['uiid']);
+    if($issue and $issue['uid'] == $user->data['uid'] ){
+      $db->query("DELETE FROM user_issue_table WHERE uiid=".$db->quote($_POST['uiid']));
+      $alert->add("Wontfix Issue","The issue has been marked as wontfix.","success");
+    } else {
+      $alert->add("Invalid Issue UIID","uiid lookup failed or owner mismatch.","error");
+    }
+  }
 }
