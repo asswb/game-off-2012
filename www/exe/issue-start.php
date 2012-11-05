f3f3dd3ca21820967b73b6e202a19f5b424672c6
Seppi <josefnpat@hotmail.com>
2012-11-05 09:15:46 -0500

    Check owner on issue start

--- www/exe/issue-start.php
+++ www/exe/issue-start.php
@@ -4,11 +4,11 @@ if(isset($user->session)){
     $alert->add("Cannot Start Issue","There is already an active issue.","info");
   } else {
     $issue = $issues->get_user_issue($_POST['uiid']);
-    if($issue){
+    if($issue and $issue['uid'] == $user->data['uid'] ){
       $issues->set_current_issue($issue['iid']);
       $alert->add("Start Issue","The issue has been started.","success");
     } else {
-      $alert->add("Invalid Issue UIID","uiid lookup failed.","error");
+      $alert->add("Invalid Issue UIID","uiid lookup failed or owner mismatch.","error");
     }
   }
 }
