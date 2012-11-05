7273e62d35d009477df4d76cc5ac701ae1cf433a
Seppi <josefnpat@hotmail.com>
2012-11-05 08:45:15 -0500

    Closes #27

--- www/page/landing.php
+++ www/page/landing.php
@@ -1,19 +1,28 @@
 <?php
 if(isset($user->session)){
 ?>
-<h2>Welcome to GitHub Tycoon!</h2>
+<h2><?php echo $user->data['repo_name']; ?></h2>
+<dl class="dl-horizontal">
+  <dt>Community</dt>
+  <dd><?php echo $user->data['com'];?> people</dd>
+  <dt>Code Base Quality</dt>
+  <dd><?php echo $user->data['cbq']*100;?>%</dd>
+  <dt>Score</dt>
+  <dd><?php echo $user->data['com']*$user->data['cbq'];?></dd>
+</dl>
+<?php
+} else {
+?>
+<h2>Welcome</h2>
+<h4>Welcome to GitHub Tycoon! Please <a href="?page=login">login</a> or <a href="?page=register">register</a> to play!</h4>
 <p>Welcome to the Beta version!</p>
 <p>Technologies used;</p>
 <ul>
 <li>PHP</li>
 <li>MySQL</li>
 <li>Twitter Bootstrap</li>
+<li>Bootswap</li>
 </ul>
-<?php
-} else {
-?>
-<h2>Welcome</h2>
-<h4>Welcome to GitHub Tycoon! Please login or register to play!</h4>
 <?php
   login_form();
 }
