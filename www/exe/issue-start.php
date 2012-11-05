b6d0772e0f299858bb3bfe4c40e1f6ac3179d754
Seppi <josefnpat@hotmail.com>
2012-11-05 07:01:49 -0500

    closes #10

--- www/exe/issue-start.php
+++ www/exe/issue-start.php
@@ -1,4 +1,14 @@
 <?php
 if(isset($user->session)){
-  $alert->add("Issue Start","The issue has been started.","success");
+  if($user->data['issue']){
+    $alert->add("Cannot Start Issue","There is already an active issue.","info");
+  } else {
+    $issue = $issues->get_user_issue($_POST['uiid']);
+    if($issue){
+      $issues->set_current_issue($issue['iid']);
+      $alert->add("Start Issue","The issue has been started.","success");
+    } else {
+      $alert->add("Invalid Issue UIID","uiid lookup failed.","error");
+    }
+  }
 }
