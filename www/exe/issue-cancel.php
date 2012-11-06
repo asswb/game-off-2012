90bc1fc5e15b0047b3e82031d6bd9ec1d42740e2
Seppi <josefnpat@hotmail.com>
2012-11-05 21:13:05 -0500

    Language cleanup

--- www/exe/issue-cancel.php
+++ www/exe/issue-cancel.php
@@ -2,11 +2,11 @@
 if(isset($user->session)){
   if($user->data['issue']){
     $issues->set_current_issue();
-    $alert->add("Issue Cancelled","Issue has been cancelled.","success");
+    $alert->add("Cancel Issue","The issue has been cancelled.","success");
   } else {
     $issue = $issues->get_user_issue($_POST['uiid']);
     if($issue and $issue['uid'] == $user->data['uid'] ){
-      $alert->add("Cannot Cancel Issue","There are no action issues.","info");
+      $alert->add("Cannot Cancel Issue","No active issue.","info");
     } else {
       $alert->add("Invalid Issue UIID","uiid lookup failed or owner mismatch.","error");
     }
