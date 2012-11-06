bc5eef84ae4887f04e2b38adc87eb9797d844d83
peterrstanley <peterrstanley@gmail.com>
2012-11-05 20:25:53 -0500

    * Removed iid from set_current_issue() function and clean up code format.

--- www/exe/issue-cancel.php
+++ www/exe/issue-cancel.php
@@ -1,12 +1,12 @@
 <?php
 if(isset($user->session)){
   if($user->data['issue']){
-    $issues->set_current_issue($issue['iid']);
+    $issues->set_current_issue();
     $alert->add("Issue Cancelled","Issue has been cancelled.","success");
   } else {
     $issue = $issues->get_user_issue($_POST['uiid']);
     if($issue and $issue['uid'] == $user->data['uid'] ){
-       $alert->add("Cannot Cancel Issue","There are no action issues.","info");
+      $alert->add("Cannot Cancel Issue","There are no action issues.","info");
     } else {
       $alert->add("Invalid Issue UIID","uiid lookup failed or owner mismatch.","error");
     }
