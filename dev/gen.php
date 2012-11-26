e479fb0e712d3904240e2efa949e16014e460e55
Seppi <josefnpat@hotmail.com>
2012-11-26 09:33:51 -0500

    Start of issue database generation. Still needs the values configured. Don't push to structure.sql until issue #42 is closed. To add the output of gen.php to structure.sql;
    1) run `./gen.php > temp`
    2) insert contents of temp into appropriate section of structure.sql
    3) change trailing , to ;
    4) update AUTO_INCREMENT to total number of generated issues + 1

---
+++ dev/gen.php
@@ -0,0 +1,140 @@
+#!/usr/bin/php
+<?php
+
+$difficulty = array(
+  "Simple" => "I have a drag and drop GUI application for this.",
+  "Beginner" => "I can\'t believe this is actually an issue.",
+  "Casual" => "I might as well drink while I do this. Vodka sounds good.",
+  "Greenhorn" => "Maybe I can play starcraft with one hand while I do this.",
+  "Novice" => "Do I even need my keyboard to fix this?",
+  "Very Easy" => "While I\'m doing this, I guess I\'ll learn DVORAK.",
+  "Rookie" => "Wow, this is really easy to fix. Even a VB coder could fix this.",
+  "Amateur" => "Ok, drag and drop won\'t fix this.",
+  "Apprentice" => "Did someone say Excel macro?",
+  "Easy" => "I guess I\'ll download 300MB of IDE to fix this.",
+  "Normal" => "This is a issue befitting of me.",
+  "Regular" => "There\'s only 2,000 lines of code to sift through.",
+  "Adept" => "This language doesn\'t have OOP. How do I make that happen?",
+  "Medium" => "The last guy got fired over this code.",
+  "Hard" => "Better get a new pot of coffee brewing.",
+  "Very Hard" => "Naming conventions? Badges? We don\'t need no stinkin\' naming conventions!",
+  "Challenging" => "I might as well beat Contra while I\'m at it.",
+  "Tough" => "Why do I need to alter the C compiler to do this?",
+  "Masochistic" => "If I write an entire IDE for this, I bet I could do it.",
+  "Obscene" => "Why were the backups stored in `/dev/null` ?",
+  "Suicidal" => "If I can\'t code this in one go, I\'m starting over.",
+  "Crippling" => "It\'s a shame I don\'t know what language this is written in. I can tell it\'s RTL.",
+  "Extra Spicy" => "If I drink an entire bottle of Rooster Sauce, I bet I could do this.",
+  "Cracker" => "I suppose I could write a back door...",
+  "Veteran" => "I will have to get my BAC between 0.129 and 0.138 percent to finish this.",
+  "Severe" => "These FPS should not be single digit.",
+  "Critical" => "I wonder how much longer I can leave this issue before it blows up.",
+  "Elite" => "I may need to leave my feet on the keyboard for a while.",
+  "Professional" => "This needs to be done in microseconds, not seconds.",
+  "Hardened" => "Can someone CC linus in on this issue?",
+  "Ultra-Violence" => "Welly, welly, welly, welly, welly, welly, well. To what do I owe the extreme pleasure of this surprising visit?",
+  "Insane" => "I could do this if I can prove Brainfuck to be turing complete.",
+  "Impossible" => "What is the fastest algorithm for multiplication of two n-digit numbers?",
+  "Heroic" => "The only way to make this work is to use an FPGA and write the code in VHDL.",
+  "Legendary" => "Whoever did this originally wrote it in Malbolge.",
+  "Mythic" => "I need to find a COBOL programmer if I\'m going to fix this.",
+  "Godlike" => "This entire library needs to be rewritten in assembly.",
+);
+
+//echo count($difficulty) . " difficulties.\n";
+
+$bug_types = array(
+  "Syntax Error" => "If you're happy and you know it, syntax error!",
+  "Compilation Error" => "Typos can be a nusiance.",
+  "Run Time Error" => "Ouch! My program didn\'t like that!",
+  "Logic Error" => "Why isn\'t this section doing what it\'s supposed to be doing?",
+  "Latent Error" => "I was not expecting this result.",
+  "Division by Zero Error" => "What do you mean you can\'t divide by zero?",
+  "Overflow Bug" => "1 + 1 = 0.",
+  "Precision Error" => "0.9999999999 != 1.",
+  "Infinite Loop" => "Why won\'t this program terminate?",
+  "Recursion Error" => "Recursion: See Recursion.",
+  "Off By One Error" => "Leave it to a computer to be unable to count.",
+  "Null Pointer Error" => "This pointer is not pointing to anything. It should be, but it isn\'t. Why isn\'t it?",
+  "Uninitialized Variable Error" => "What the hell is `i_like_to_move_it_move_it`, and why isn\'t it defined?",
+  "Type Error" => "What do you mean I can\'t divide strings by a number?",
+  "Segfault" => "Who the hell is trying to push the binary into kernel space?",
+  "Memory Leak" => "Memory is not a black hole.",
+  "Buffer Overflow" => "You shall not pass!",
+  "Stack Overflow" => "Pushing but not popping.",
+  "Deadlock" => "Why do mute philosophers need to eat with two forks anyway?",
+  "Race Condition" => "Remove seatbelt, then leave the car.",
+  "Incorrect API usage" => "api.exceptIncomingTransmission()",
+  "Incorrect Protocol Implementation" => "Do I *need* HTML to make XML?",
+  "Incorrect Hardware Handling" => "Why does this function play the imperial march on my floppy drive?",
+  "Performance Issue" => "This process may take anywhere from 0.01 to 9000 seconds.",
+  "Random Disk Access Issue" => "Why does this library use twelve thousand individual files?",
+  "Unpropagated Updates" => "Why subtract when you can just add a negative number?",
+  "Comments Illegible" => "//Has anyone really been far even as decided to use even go want to do look more like?",
+  "Documentation Issue" => "Moths do not belong in the relay.",
+  "Hard Coded" => "This needs to be dynamic.",
+);
+
+//echo count($bug_types) . " bug types.\n";
+
+$enhancement_types = array(
+  "Business Request" => "Quite a few companies would love to see this happen.",
+  "Personal Request" => "Not many people will benefit from this, but it is rather good idea.",
+  "API" => "If we can build an API, we can allow folks to connect to it.",
+  "User Interface" => "People like to click buttons. Let\'s add those!",
+  "Command Line Interface" => "Let\'s make our system script-able!",
+  "Daemon" => "Our system needs to run all the time!",
+  "Database" => "Our information needs to be persistent.",
+  "Threading" => "Our system needs to take advantage of multi-core processors.",
+  "Documentation" => "RTFM? WABFM.",
+  "Hardware Integration" => "We need to add a dongle.",
+  "Automated Tests" => "I\'m tired of testing.",
+  "Makefile" => "When you have eight cores, it\'s nice to have the `-j` flag.",
+  "Deploy Scripts" => "We need to have a public instance.",
+  "Backup Scripts" => "Keeping stuff stored in /dev/null isn\'t going to work anymore.",
+  "Internal Logic" => "Logic will get you from A to B. Imagination will take you everywhere.",
+  "Third Party Library" => "Why reinvent the wheel?",
+  "Website" => "We need a design that POPS! Maybe even comic sans!",
+  "Forums" => "Calling all trolls.",
+  "IRC Channel" => "Old enough to keep the technically challenged out.",
+  "Phone App" => "I need to access this from my phone. Now.",
+);
+
+//echo count($enhancement_types) . " enhancement types.\n";
+
+$pull_request_types = array(
+  "Patch" => "Someone attached a patch on the issue tracker!",
+  "Feature" => "Someone wrote some code and posted it!",
+  "Bugfix" => "Someone figured out the problem to a bug and shows how to fix it!",
+  "Performance Boost" => "Someone was able to make your system faster!",
+);
+
+//echo count($pull_request_types) . " pull request types.\n";
+
+function print_mysql($count,$name,$description,$type,$time,$delta_com,$delta_cbq,$chance){
+  echo "($count, '$name', '$description', '$type', $time, $delta_com, $delta_cbq, $chance),\n";
+}
+
+$count = 0;
+foreach($difficulty as $dkey => $dvalue){
+  foreach($bug_types as $bkey => $bvalue){
+    $count++;
+    print_mysql($count,"$dkey $bkey","$bvalue $dvalue",'bug',1,1,1,1);
+  }
+  foreach($enhancement_types as $ekey => $evalue){
+    $count++;
+    print_mysql($count,"$dkey $ekey","$evalue $dvalue",'enhancement',1,1,1,1);
+  }
+  foreach($pull_request_types as $pkey => $pvalue){
+    $count++;
+    print_mysql($count,"$dkey $pkey","$pvalue $dvalue",'pull_request',1,1,1,1);
+  }
+}
+
+// Custom issues
+$count++;
+print_mysql($count,"Code Rebase","Rewrite major parts of the core and update to newest specifications.", 'enhancement', 240, 10, -2, 100);
+$count++;
+print_mysql($count,'Code Refactor', 'Refactor a segment of the code.', 'enhancement', 60, 0, 2, 100);
+$count++;
+print_mysql($count,'Color Change', '<pre>git rm -rf *\r\ngit commit -m "Fixed all bugs"\r\ngit push</pre>', 'pull_request', 2, -10, -10, 1);
--- structure.sql
+++ structure.sql
@@ -35,23 +35,66 @@ CREATE TABLE IF NOT EXISTS `issue_table` (
   `delta_cbq` int(11) NOT NULL,
   `chance` int(3) NOT NULL,
   UNIQUE KEY `iid` (`iid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=54 ;
 
 --
 -- Dumping data for table `issue_table`
 --
 
 INSERT INTO `issue_table` (`iid`, `name`, `description`, `type`, `time`, `delta_com`, `delta_cbq`, `chance`) VALUES
-(1, 'Simple Bug', 'This one isn''t very hard, and not very rewarding.', 'bug', 20, 1, 2, 8),
-(2, 'Intermediate Bug', 'This one is harder.', 'bug', 40, 3, 2, 4),
-(3, 'Hard Bug', 'This is a tricky one. Better hunker down.', 'bug', 60, 5, 2, 2),
-(4, 'Brutal Bug', 'This bug is super hard, oh god no!', 'bug', 80, 7, 2, 1),
-(5, 'Can you add this feature?', 'My company would switch if you had this feature!', 'enhancement', 20, 10, -2, 2),
-(6, 'Has anyone really been far even as decided to use even go want t', '<pre>git rm -rf *\r\ngit commit -m "Fixed all bugs"\r\ngit push</pre>', 'pull_request', 2, -10, -10, 1),
-(7, 'Hey, I found a bug!', 'Attached is a diff.', 'pull_request', 5, 10, -1, 1),
-(8, 'Hey, you''re missing this feature!', 'Here''s a patch!', 'pull_request', 5, 8, -2, 2),
-(9, 'Code Rebase', 'Rewrite major parts of the core and update to newest specifications.', 'enhancement', 240, 10, -2, 100),
-(10, 'Refactor', 'Refactor a segment of the code.', 'enhancement', 60, 0, 2, 100);
+(1, 'Simple Syntax Error', 'If you're happy and you know it, syntax error! I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(2, 'Simple Compilation Error', 'Typos can be a nusiance. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(3, 'Simple Run Time Error', 'Ouch! My program didn\'t like that! I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(4, 'Simple Logic Error', 'Why isn\'t this section doing what it\'s supposed to be doing? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(5, 'Simple Latent Error', 'I was not expecting this result. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(6, 'Simple Division by Zero Error', 'What do you mean you can\'t divide by zero? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(7, 'Simple Overflow Bug', '1 + 1 = 0. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(8, 'Simple Precision Error', '0.9999999999 != 1. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(9, 'Simple Infinite Loop', 'Why won\'t this program terminate? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(10, 'Simple Recursion Error', 'Recursion: See Recursion. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(11, 'Simple Off By One Error', 'Leave it to a computer to be unable to count. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(12, 'Simple Null Pointer Error', 'This pointer is not pointing to anything. It should be, but it isn\'t. Why isn\'t it? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(13, 'Simple Uninitialized Variable Error', 'What the hell is `i_like_to_move_it_move_it`, and why isn\'t it defined? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(14, 'Simple Type Error', 'What do you mean I can\'t divide strings by a number? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(15, 'Simple Segfault', 'Who the hell is trying to push the binary into kernel space? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(16, 'Simple Memory Leak', 'Memory is not a black hole. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(17, 'Simple Buffer Overflow', 'You shall not pass! I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(18, 'Simple Stack Overflow', 'Pushing but not popping. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(19, 'Simple Deadlock', 'Why do mute philosophers need to eat with two forks anyway? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(20, 'Simple Race Condition', 'Remove seatbelt, then leave the car. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(21, 'Simple Incorrect API usage', 'api.exceptIncomingTransmission() I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(22, 'Simple Incorrect Protocol Implementation', 'Do I *need* HTML to make XML? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(23, 'Simple Incorrect Hardware Handling', 'Why does this function play the imperial march on my floppy drive? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(24, 'Simple Performance Issue', 'This process may take anywhere from 0.01 to 9000 seconds. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(25, 'Simple Random Disk Access Issue', 'Why does this library use twelve thousand individual files? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(26, 'Simple Unpropagated Updates', 'Why subtract when you can just add a negative number? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(27, 'Simple Comments Illegible', '//Has anyone really been far even as decided to use even go want to do look more like? I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(28, 'Simple Documentation Issue', 'Moths do not belong in the relay. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(29, 'Simple Hard Coded', 'This needs to be dynamic. I have a drag and drop GUI application for this.', 'bug', 1, 1, 1, 1),
+(30, 'Simple Business Request', 'Quite a few companies would love to see this happen. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(31, 'Simple Personal Request', 'Not many people will benefit from this, but it is rather good idea. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(32, 'Simple API', 'If we can build an API, we can allow folks to connect to it. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(33, 'Simple User Interface', 'People like to click buttons. Let\'s add those! I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(34, 'Simple Command Line Interface', 'Let\'s make our system script-able! I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(35, 'Simple Daemon', 'Our system needs to run all the time! I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(36, 'Simple Database', 'Our information needs to be persistent. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(37, 'Simple Threading', 'Our system needs to take advantage of multi-core processors. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(38, 'Simple Documentation', 'RTFM? WABFM. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(39, 'Simple Hardware Integration', 'We need to add a dongle. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(40, 'Simple Automated Tests', 'I\'m tired of testing. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(41, 'Simple Makefile', 'When you have eight cores, it\'s nice to have the `-j` flag. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(42, 'Simple Deploy Scripts', 'We need to have a public instance. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(43, 'Simple Backup Scripts', 'Keeping stuff stored in /dev/null isn\'t going to work anymore. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(44, 'Simple Internal Logic', 'Logic will get you from A to B. Imagination will take you everywhere. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(45, 'Simple Third Party Library', 'Why reinvent the wheel? I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(46, 'Simple Website', 'We need a design that POPS! Maybe even comic sans! I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(47, 'Simple Forums', 'Calling all trolls. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(48, 'Simple IRC Channel', 'Old enough to keep the technically challenged out. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(49, 'Simple Phone App', 'I need to access this from my phone. Now. I have a drag and drop GUI application for this.', 'enhancement', 1, 1, 1, 1),
+(50, 'Simple Patch', 'Someone attached a patch on the issue tracker! I have a drag and drop GUI application for this.', 'pull_request', 1, 1, 1, 1),
+(51, 'Simple Feature', 'Someone wrote some code and posted it! I have a drag and drop GUI application for this.', 'pull_request', 1, 1, 1, 1),
+(52, 'Simple Bugfix', 'Someone figured out the problem to a bug and shows how to fix it! I have a drag and drop GUI application for this.', 'pull_request', 1, 1, 1, 1),
+(53, 'Simple Performance Boost', 'Someone was able to make your system faster! I have a drag and drop GUI application for this.', 'pull_request', 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
