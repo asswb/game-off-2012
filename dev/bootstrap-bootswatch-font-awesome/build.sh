79af2bfb14a65f7112af1d1b87a3b094719e176f
Seppi <josefnpat@hotmail.com>
2012-11-26 12:57:00 -0500

    Make the build a little more dynamic

--- dev/bootstrap-bootswatch-font-awesome/build.sh
+++ dev/bootstrap-bootswatch-font-awesome/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+BOOTSWATCH='spacelab'
+
 git clone https://github.com/twitter/bootstrap.git
 
 cd bootstrap/less/
@@ -12,10 +14,10 @@ cp FortAwesome-Font-Awesome-*/font . -Rv
 cp FortAwesome-Font-Awesome-*/less/font-awesome.less . -v
 
 rm variables.less
-wget http://bootswatch.com/spacelab/variables.less
+wget http://bootswatch.com/$BOOTSWATCH/variables.less
 
 rm bootswatch.less
-wget http://bootswatch.com/spacelab/bootswatch.less
+wget http://bootswatch.com/$BOOTSWATCH/bootswatch.less
 
 patch -p0 < ../../bootstrap.less.patch
 patch -p0 < ../../font-awesome.less.patch
