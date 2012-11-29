7797dc4a36881b8ca2fedb5bab69c0310b8107f2
Seppi <josefnpat@hotmail.com>
2012-11-29 08:40:18 -0500

    Adding Help page

---
+++ www/page/help.php
@@ -0,0 +1,37 @@
+<h1>Help</h1>
+<p>GitHub Tycoon is a persistent software repository management simulation game.</p>
+<h2>Glossary</h2>
+  <p>CBQ .. Code Base Quality</p>
+  <p>COM .. Community</p>
+<h2>FAQ</h2>
+<h3>What do the different issues do?</h3>
+  <b>Issues effect your CBQ and COM both positively and negatively.</b> Here's the lowdown;
+  <ul>
+    <li>
+      <h4>Enhancements</h4>
+      <p>Upon completion, these issues will give you +2% to CBQ and +COM.</p>
+    </li>
+    <li>
+      <h4>Bugs</h4>
+      <p>Upon discovery, these issues will give you -2% to CBQ.</p>
+      <p>Upon completion, these issues will give you +2% to CBQ and +COM.</p>
+    </li>
+    <li>
+      <h4>Pull Requests</h4>
+      <p>Upon completion, these issues will give you -10% to CBQ, but a large +COM.</p>
+    </li>
+  </ul>
+<h3>Why can't I get to 100% CBQ?</h3>
+ <p>For every bug you have, -2% is subtracted off of your 100% maximum CBQ.</p>
+<h3>My score is zero! How is score calculated?</h3>
+  <p>Score = CBQ * COM</p>
+<h3>Bugs, Questions, Suggestions, Rude Comments?</h3>
+  <p>If you have questions, feel free to stop by and ask on the chat page.</p>
+  <p>If you find a bug;
+    <ul>
+      <li>and can reproduce it, feel free to mosey on over to the <a href="https://github.com/asswb/game-off-2012/issues">Issues page</a> at GitHub.</li>
+      <li>and cannot reproduce it, talk to the folks in chat to see if anyone else can.</li>
+    </ul>
+  </p>
+  <p>If you have suggestions, and they haven't already been suggested, either talk to the folks in chat or post it to the <a = href="https://github.com/asswb/game-off-2012/issues">Issues page</a> at GitHub.</p>
+  <p>If you have rude comments, feel free to stop by and chat!</p>
